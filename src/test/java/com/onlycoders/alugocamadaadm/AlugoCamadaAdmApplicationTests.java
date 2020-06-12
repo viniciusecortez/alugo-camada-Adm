9703def2a230df7e1fb8fbe5883c4e4361c43456
Dilan Lima <dilan_limacampos@hotmail.com>
2020-06-11 22:26:00 -0300

    commit inicial do projeto

---
+++ src/test/java/com/onlycoders/alugocamadaadm/AlugoCamadaAdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlycoders.alugocamadaadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugoCamadaAdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
